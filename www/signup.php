c3891993a76c5e7d9725712296e87b622160a55f
root <bernie6401@gmail.com>
2022-05-10 05:15:07 +0000

    revise the background logic of login

--- www/signup.php
+++ www/signup.php
@@ -11,146 +11,132 @@
 
     <body class="container container-adjust text-white bg-dark">
         <div class="form-floating form-floating-adjust padding-left-mid">
-        <!-- <form>
-            <input class="form-control input-adjust" id="floatingInput" type="text" name="username" id="username" placeholder="Email">
-            </br>
-            <input class="form-control input-adjust" id="floatingInput" type="text" name="username" id="username" placeholder="Full Name">
-            </br>
-            <input class="form-control input-adjust" id="floatingInput" type="text" name="username" id="username" placeholder="Username">
-            </br>
-            <input class="form-control input-adjust" id="floatingInput" type="password" name="password" id="password" placeholder="Password">
-            </br>
-            <fieldset disabled="" aria-label="Disabled fieldset example">
-                <input class="form-control input-adjust" type="submit" name="submit_bt" id="submit_bt" value="Sign up" class="btn btn-primary">
-            </fieldset>
-        </form> -->
+            <form class="row g-3 needs-validation" novalidate id="formAdd" name="formAdd" method="post">
+                <div class="margin-top-div-adjust">
+                    <label for="validationCustom01">
+                        First name
+                        <span class="required-star-adjust">*</span>
+                    </label>
+                    <input type="text" class="margin-top-adjust form-control" id="validationCustom01" required>
+                    <div class="valid-feedback">
+                    Looks good!
+                    </div>
+                </div> 
 
-        <form class="row g-3 needs-validation" novalidate id="formAdd" name="formAdd" method="post">
-            <div class="margin-top-div-adjust">
-                <label for="validationCustom01" class="">
-                    First name
-                    <span class="required-star-adjust">*</span>
-                </label>
-                <input type="text" class="margin-top-adjust form-control" id="validationCustom01" value="" required>
-                <div class="valid-feedback">
-                Looks good!
-                </div>
-            </div>
-
-            <div class="margin-top-div-adjust">
-                <label for="validationCustom02" class="">
-                    Last name
-                    <span class="required-star-adjust">*</span>
-                </label>
-                <input type="text" class="form-control" id="validationCustom02" value="" required placeholder="Last name">
-                <div class="valid-feedback">
-                Looks good!
-                </div>
-            </div>
-
-            <div class="margin-top-div-adjust">
-                <label for="validationCustomUsername" class="">
-                    Username
-                    <span class="required-star-adjust">*</span>
-                </label>
-                <div class="input-group has-validation">
-                    <span class="input-group-text" id="inputGroupPrepend">@</span>
-                    <input type="text" class="form-control" id="validationCustomUsername" aria-describedby="inputGroupPrepend" required placeholder="Username">
-                    <div class="invalid-feedback">
-                        Please choose a username.
+                <div class="margin-top-div-adjust">
+                    <label for="validationCustom02">
+                        Last name
+                        <span class="required-star-adjust">*</span>
+                    </label>
+                    <input type="text" class="form-control" id="validationCustom02" required>
+                    <div class="valid-feedback">
+                    Looks good!
                     </div>
                 </div>
-            </div>
 
-            <div class="margin-top-div-adjust">
-                <label for="validationCustomPassword" class="">
-                    Password
-                    <span class="required-star-adjust">*</span>
-                </label>
-                <input type="password" class="margin-top-adjust form-control" id="validationCustomPassword" value="" required name="password" id="password">
-
-                <input class="form-check-input" type="checkbox" onclick="myFunction()">
-                <label class="form-check-label">
-                    <p>Show Password</p>
-                </label>
-                <div class="valid-feedback">
-                    Looks good!
+                <div class="margin-top-div-adjust">
+                    <label for="validationCustomUsername">
+                        Username
+                        <span class="required-star-adjust">*</span>
+                    </label>
+                    <div class="input-group has-validation">
+                        <span class="input-group-text" id="inputGroupPrepend">@</span>
+                        <input class="form-control" id="validationCustomUsername" type="text" name="username" aria-describedby="inputGroupPrepend" required>
+                        <div class="invalid-feedback">
+                            Please choose a username.
+                        </div>
+                    </div>
                 </div>
-            </div>
 
-            <div class="margin-top-div-adjust">
-                <label for="validationCustomEmail" class="">
-                    Email
-                    <span class="required-star-adjust">*</span>
-                </label>
-                <input type="text" class="margin-top-adjust form-control" id="validationCustomEmail" value="" required name="username" id="username">
-                <div class="valid-feedback">
-                    Looks good!
+                <div class="margin-top-div-adjust">
+                    <label for="validationCustomPassword">
+                        Password
+                        <span class="required-star-adjust">*</span>
+                    </label>
+                    <input class="margin-top-adjust form-control" id="validationCustomPassword" type="password" name="password" required>
+                    
+                    <input class="form-check-input" type="checkbox" onclick="myFunction()">
+                    <label class="form-check-label">
+                        <p>Show Password</p>
+                    </label>
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
                 </div>
-            </div>
 
-            <div class="margin-top-div-adjust">
-                <label for="validationCustom04" class="">
-                    State
-                    <span class="required-star-adjust">*</span>
-                </label>
-                <select class="form-select" id="validationCustom04" required placeholder="State">
-                    <option selected disabled value="">City Choose...</option>
-                    <option>Keelung City</option>
-                    <option>Taipei City</option>
-                    <option>New Taipei City</option>
-                    <option>Taoyuan City</option>
-                    <option>Hsinchu County</option>
-                    <option>Hsinchu City</option>
-                    <option>Miaoli County</option>
-                    <option>Taichung City</option>
-                    <option>Changhua Country</option>
-                    <option>Nantou County</option>
-                    <option>Yunlin County</option>
-                    <option>Chiayi County</option>
-                    <option>Chiayi City</option>
-                    <option>Tainan City</option>
-                    <option>Kaohsiung City</option>
-                    <option>Pingtung County</option>
-                    <option>Taitung County</option>
-                    <option>Hualien County</option>
-                    <option>Yilan County</option>
-                    <option>Penghu County</option>
-                    <option>Kinmen County</option>
-                    <option>Lienchiang County</option>
-                </select>
-                <div class="invalid-feedback">
-                    Please select a valid city.
+                <div class="margin-top-div-adjust">
+                    <label for="validationCustomEmail">
+                        Email
+                        <span class="required-star-adjust">*</span>
+                    </label>
+                    <input type="text" class="margin-top-adjust form-control" id="validationCustomEmail" required name="email">
+                    <div class="valid-feedback">
+                        Looks good!
+                    </div>
                 </div>
-            </div>
 
-            <div class="margin-top-div-adjust">
-                <label for="validationCustom05" class="">
-                    Zip
-                    <span class="required-star-adjust">*</span>
-                </label>
-                <input type="text" class="form-control" id="validationCustom05" required>
-                <div class="invalid-feedback">
-                    Please provide a valid zip.
+                <div class="margin-top-div-adjust">
+                    <label for="validationCustom04">
+                        State
+                        <span class="required-star-adjust">*</span>
+                    </label>
+                    <select class="form-select" id="validationCustom04" required placeholder="State">
+                        <option selected disabled>City Choose...</option>
+                        <option>Keelung City</option>
+                        <option>Taipei City</option>
+                        <option>New Taipei City</option>
+                        <option>Taoyuan City</option>
+                        <option>Hsinchu County</option>
+                        <option>Hsinchu City</option>
+                        <option>Miaoli County</option>
+                        <option>Taichung City</option>
+                        <option>Changhua Country</option>
+                        <option>Nantou County</option>
+                        <option>Yunlin County</option>
+                        <option>Chiayi County</option>
+                        <option>Chiayi City</option>
+                        <option>Tainan City</option>
+                        <option>Kaohsiung City</option>
+                        <option>Pingtung County</option>
+                        <option>Taitung County</option>
+                        <option>Hualien County</option>
+                        <option>Yilan County</option>
+                        <option>Penghu County</option>
+                        <option>Kinmen County</option>
+                        <option>Lienchiang County</option>
+                    </select>
+                    <div class="invalid-feedback">
+                        Please select a valid city.
+                    </div>
                 </div>
-            </div>
 
-            <div class="margin-top-div-adjust">
-                <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="invalidCheck" required>
-                <label class="form-check-label" for="invalidCheck">
-                    <p>By creating an account you agree to our <a href="./TermsPrivacy/google_terms_of_service_en.pdf" style="color:dodgerblue">Terms & Privacy</a>.</p>
-                </label>
-                <div class="invalid-feedback">
-                    You must agree before submitting.
+                <div class="margin-top-div-adjust">
+                    <label for="validationCustom05">
+                        Zip
+                        <span class="required-star-adjust">*</span>
+                    </label>
+                    <input type="text" class="form-control" id="validationCustom05" required>
+                    <div class="invalid-feedback">
+                        Please provide a valid zip.
+                    </div>
                 </div>
-                </div>
-            </div>
 
-            <div class="margin-top-div-adjust">
-                <button class="float-right-adjust btn btn-primary" type="submit">Submit form</button>
-            </div>
-        </form>
+                <div class="margin-top-div-adjust">
+                    <div class="form-check">
+                    <input class="form-check-input" type="checkbox" id="invalidCheck" required>
+                    <label class="form-check-label" for="invalidCheck">
+                        <p>By creating an account you agree to our <a href="./TermsPrivacy/google_terms_of_service_en.pdf" style="color:dodgerblue">Terms & Privacy</a>.</p>
+                    </label>
+                    <div class="invalid-feedback">
+                        You must agree before submitting.
+                    </div>
+                    </div>
+                </div>
+
+                <div class="margin-top-div-adjust">
+                    <button class="float-right-adjust btn btn-primary" type="submit" name="submit_bt">Submit form</button>
+                </div>
+            </form>
 
         </div>
 
@@ -162,12 +148,13 @@
 <?php
     if(isset($_POST['username']) && isset($_POST['password']) && $_POST['username']!="" && $_POST['password']!="")
     {
+        echo '<script>alert("GGG")</script>';
         //must check the username include sqli words or not
         require_once('sqli_filter.php');
         $sqli_username_detect = sqli_detect_signup($_POST['username']);
         if($sqli_username_detect === true)
         {
-            echo 'You can not use the special character.';
+            echo '<script>alert("You can not use the special character.")</script>';
         }
         else
         {
@@ -176,9 +163,9 @@
             {
                 if($row)
                 {
-                    echo 'Oops, you signed up before, please login at home page...<br><br>';
+                    echo '<script>alert("Oops, you signed up before, please login at home page...")</script>';
                     mysqli_close($db_link);
-                    header("refresh:3; url=index.php");
+                    header("refresh:1; url=index.php");
                 }
                 else
                 {
@@ -197,8 +184,8 @@
                     $_SESSION['password'] = $password;
 
                     //head to home page
-                    echo 'Sign up successful. Please wait 3s for redirection.<br><br>';
-                    header("refresh:3; url=index.php");
+                    echo '<script>alert("Sign up successful. Please wait 1s for redirection.")</script>';
+                    header("refresh:1; url=index.php");
                 }
             }
             catch (Exception $e)
