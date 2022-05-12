82816722a41514cd80f451cdd809bd71a3b12bc5
root <bernie6401@gmail.com>
2022-05-12 10:39:33 +0000

    change the error message type to js alert from echo type

--- www/check_upload_data.php
+++ www/check_upload_data.php
@@ -1,8 +1,6 @@
 <?php 
-    ob_start();
     session_save_path('/var/www/html/session_data/');
     session_start();
-    include 'style.html';
     
     $redirect_addr = "index.php";
     //verify file extension
@@ -15,8 +13,8 @@
     //verify image type
     if(isset($_FILES['image_file']) && !is_readable($_FILES['image_file']['tmp_name']))
     {
-        echo 'You can not upload files which size over 1M!';
-        header("refresh:3; url=$redirect_addr");
+        echo '<script>alert("You can not upload files which size over 1M!")</script>';
+        header("refresh:1; url=$redirect_addr");
     }
     else
         list($_, $_, $type) = getimagesize($_FILES['image_file']['tmp_name']);
@@ -24,44 +22,44 @@
     
     if (!isset($_FILES['image_file']))
     {
-        echo 'Give me a file!<br>';
-        header("refresh:3; url=$redirect_addr");
+        echo '<script>alert("Give me a file!")</script>';
+        header("refresh:0.1; url=$redirect_addr");
     }
     else if (!in_array($extension, ['png', 'jpeg', 'jpg']) !== false)   //verify file extension
     {
-        echo "Invalid file type1<br>";
-        header("refresh:3; url=$redirect_addr");
+        echo '<script>alert("Invalid file type1")</script>';
+        header("refresh:0.1; url=$redirect_addr");
     }
     else if (in_array($_FILES['image_file']['type'], ["image/png", "image/jpeg", "image/jpg"]) === false)   //verify file type
     {
-        echo "Invalid file type2<br>";
-        header("refresh:3; url=$redirect_addr");
+        echo '<script>alert("Invalid file type2")</script>';
+        header("refresh:0.1; url=$redirect_addr");
     }
     else if (in_array(mime_content_type($_FILES["image_file"]["tmp_name"]), ["image/png", "image/jpeg", "image/jpg"]) === false)   //verify file type
     {
-        echo "Invalid file type3<br>";
-        header("refresh:3; url=$redirect_addr");
+        echo '<script>alert("Invalid file type3")</script>';
+        header("refresh:0.1; url=$redirect_addr");
     }
     else if ($type !== IMAGETYPE_JPEG && $type !== IMAGETYPE_PNG)
     {
-        echo "Invalid file type4<br>";
-        header("refresh:3; url=$redirect_addr");
+        echo '<script>alert("Invalid file type4")</script>';
+        header("refresh:0.1; url=$redirect_addr");
     }
     else if(($type == IMAGETYPE_PNG || 
             mime_content_type($_FILES["image_file"]["tmp_name"]) == "image/png" || 
             $_FILES['image_file']['type'] == "image/png") && 
             !imagecreatefrompng($_FILES['image_file']['tmp_name']))
     {
-        echo "Invalid file type5<br>";
-        header("refresh:3; url=$redirect_addr");
+        echo '<script>alert("Invalid file type5")</script>';
+        header("refresh:0.1; url=$redirect_addr");
     }
     else if(($type == IMAGETYPE_JPEG || 
             in_array(mime_content_type($_FILES["image_file"]["tmp_name"]), ["image/jpeg", "image/jpg"]) === true || 
             in_array($_FILES['image_file']['type'], ["image/jpeg", "image/jpg"]) === true) && 
             !imagecreatefromjpeg($_FILES['image_file']['tmp_name']))
     {
-        echo "Invalid file type6<br>";
-        header("refresh:3; url=$redirect_addr");
+        echo '<script>alert("Invalid file type6")</script>';
+        header("refresh:0.1; url=$redirect_addr");
     }
     else if ($_FILES['image_file']['error'] === UPLOAD_ERR_OK)
     {
@@ -74,7 +72,7 @@
         
         if(move_uploaded_file($_FILES['image_file']['tmp_name'], $filename))
         {
-            echo "<br>Upload Success.";
+            echo '<script>alert("Upload Success.")</script>';
 
             //revise database
             require_once('config.php');
@@ -82,11 +80,11 @@
             $username = $_POST["name"];
             $sql = "UPDATE `users_info` SET `avatar_id` = '$filename' WHERE `users_info`.`username` = '$username';";
             mysqli_query($db_link, $sql);
+            header("refresh:0.1; url=board.php");
         }
         else
-            echo '<br>Upload Failed!';
-        header("refresh:3; url=$redirect_addr");
+            echo '<script>alert("Upload Failed!")</script>';
+        header("refresh:0.1; url=$redirect_addr");
     }
     else echo 'Error code：' . $_FILES['my_file']['error'] . '<br/>';
 ?>
-<?ob_end_flush();?>
