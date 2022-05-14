4e0a36123f27d347e2139275e083d022aefae29a
root <bernie6401@gmail.com>
2022-05-14 13:18:00 +0000

    revise the if statement

--- www/delete_post.php
+++ www/delete_post.php
@@ -1,19 +1,15 @@
 <?php
-    ob_start();
     include("config.php");
     $db_link = ConnectDB();
 
-    if(isset($_POST['delete_post']))# && ($_POST['user_name'] == $_GET['username'])
+    if(isset($_POST['post_id']))# && ($_POST['user_name'] == $_GET['username'])
     {
         $post_time = str_replace("*", " ", $_POST["post_time"]);
         $username = $_POST["user_name"];
         $post_id = $_POST['post_id'];
         $sql_query_delete = "DELETE FROM `users_blog` WHERE `post_time` = '$post_time' and `user_name` = '$username' and `post_id` = '$post_id';";
         mysqli_query($db_link, $sql_query_delete);
-        echo $post_time;
-        echo "success";
     }
 
     header("Location: board.php");
 ?>
-<?ob_end_flush();?>
